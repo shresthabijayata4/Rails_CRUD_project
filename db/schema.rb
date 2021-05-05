059514e0ad433c162cdb7d210b4c375d3ae0f2b5
shresthabijayata4 <bijju.stha4@gmail.com>
2021-05-05 12:36:34 +0545

    change in postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 2021_05_04_090610) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "publish", default: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "cover_filename"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
